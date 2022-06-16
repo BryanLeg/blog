7a2db4f39744b0e6660ab98f8886520bae0ac637
BryanLeg <Brylegrain@gmail.com>
2022-06-16 22:01:18 +0200

    update login, authentification WIP

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,44 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [admins, setAdmins] = useState();
-  const mail = "Brylegrain@gmail.com";
+  const [errorCredentials, setErrorCredentials] = useState();
+  const navigate = useNavigate();
 
-  const fetchAdmins = async () => {
-    const { data } = await axios.post("http://localhost:5000/api/v1/admins/", {
-      mail,
-    });
-    setAdmins(data);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setErrorCredentials("please provide email and password");
+      setTimeout(() => {
+        setErrorCredentials();
+      }, 3000);
+    } else {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/v1/admins/",
+        {
+          email,
+          password,
+        }
+      );
+
+      if (data.isMatch) {
+        setErrorCredentials(data.msg);
+        navigate("/");
+      } else {
+        setErrorCredentials(data.msg);
+      }
+      setEmail("");
+      setPassword("");
+    }
   };
 
-  console.log(admins);
-
-  useEffect(() => {
-    fetchAdmins();
-  }, []);
-
   return (
-    <form className="form">
-      <input
-        className="form-input"
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="type your email"
-      />
-      <br />
-      <input
-        className="form-input"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="type your password"
-      />
-      <button className="btn">Submit</button>
-    </form>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="form-input"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="type your email"
+        />
+        <br />
+        <input
+          className="form-input"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="type your password"
+        />
+        <button className="btn">Submit</button>
+      </form>
+      <span>
+        <p className="form-alert">{errorCredentials}</p>
+      </span>
+    </>
   );
 };
 
